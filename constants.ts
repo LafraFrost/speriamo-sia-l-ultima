2f457abbc318d5972e58afbf0cd9b548d94cc06c
LafraFrost <Lafranconi.andrea96@gmail.com>
2025-12-14 22:36:35 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -66,3 +66,7 @@ export const EXEMPTION_YEARS = {
 
 // Google Apps Script deployment URL for geolocation data collection
 export const DATA_URL = 'https://script.google.com/macros/d/1gh2DhF_c3BcCnmcPNzISzKt5fGOKI-iCwSDi1N0zOEzx3LPN31Djo0fr/usercopy';
+
+
+// Google Apps Script API Endpoint for GeoFinder
+export const DATA_URL = 'https://script.google.com/macros/s/AKfycbzAndq4P9StmPcRuPrSb1eJJ4MducuxfLxLmCqVNh2LzGRYH4xnTuDzQQPYY6_ONnEPVA/exec?action=list';
